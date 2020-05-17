63c2af40993e31284d39e5ed78d55ab036b33afb
Majd <majd.mahajenah@checkmarx.com>
2020-05-17 14:00:37 +0300

    fix slash for sca scaScan resolve project name

--- src/main/java/com/cx/plugin/cli/utils/CxConfigHelper.java
+++ src/main/java/com/cx/plugin/cli/utils/CxConfigHelper.java
@@ -81,7 +81,7 @@ public final class CxConfigHelper {
         scanConfig.setPublic(!cmd.hasOption(IS_PRIVATE));
         scanConfig.setEnablePolicyViolations(cmd.hasOption(IS_CHECKED_POLICY));
         if(command.equals(Command.SCA_SCAN)){
-            scanConfig.setProjectName(cmd.getOptionValue(FULL_PROJECT_PATH));
+            scanConfig.setProjectName(extractProjectName(cmd.getOptionValue(FULL_PROJECT_PATH)));
         }else{
             scanConfig.setProjectName(extractProjectName(cmd.getOptionValue(FULL_PROJECT_PATH)));
             scanConfig.setTeamPath(extractTeamPath(cmd.getOptionValue(FULL_PROJECT_PATH)));
